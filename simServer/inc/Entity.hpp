8d461829827ccf201a82e9ee505d7a253a33e81d
chgibb <chris.gibb@outlook.com>
2017-01-23 15:05:02 -0500

    Add stats class

--- simServer/inc/Entity.hpp
+++ simServer/inc/Entity.hpp
@@ -2,10 +2,24 @@
 #include <string>
 namespace Schwarma
 {
+    class Stats
+    {
+        public:
+            Stats()=default;
+            ~Stats()=default;
+            int health = 0;
+            int resistanceToDamage = 0;
+            int resistanceToKinetic = 0;
+            int resistanceToFire = 0;
+            int resistanceToIce = 0;
+            int resistanceToEarth = 0;
+            int movementSpeed = 0;
+    };
     class Entity
     {
         public:
             int position = 0;
+            Schwarma::Stats baseStats;
             virtual bool loadFromSource(std::string&) = 0;
     };
 }
