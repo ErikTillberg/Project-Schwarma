fe875dfc8ee15ef366561705147ff490965be729
chgibb <chris.gibb@outlook.com>
2017-01-23 16:47:54 -0500

    Space out statement blocks

--- simServer/main.cpp
+++ simServer/main.cpp
@@ -14,12 +14,14 @@ int main(int argc,char*argv[])
     //Setup some dummy player objects to test with
     Schwarma::Player player1;
     Schwarma::Player player2;
+    
     player1.stats.health = 10;
     player2.stats.health = 10;
 
     //Set players on opposite ends of the stage
     player1.position = 1;
     player2.position = 7;
+
     player1.name = "Player 1";
     player2.name = "Player 2";
     //Setup a new simulation with our two players
