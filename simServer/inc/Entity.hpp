911c41fadd818e392b8d2f1110aea650461ae8bf
chgibb <chris.gibb@outlook.com>
2017-01-23 15:13:55 -0500

    Preliminary action method

--- simServer/inc/Entity.hpp
+++ simServer/inc/Entity.hpp
@@ -34,7 +34,18 @@ namespace Schwarma
             virtual bool loadFromSource(std::string&) = 0;
             int doAction()
             {
+                //Algorithm based on answer by jephthah
+                //https://www.daniweb.com/programming/software-development/threads/269329/weighted-dice
                 float percent = (float)(rand()%1024)/1024;
+                if(percent < this->behaviours.actions[Schwarma::ATTACK])
+                    return Schwarma::ATTACK;
+                if(percent < (this->behaviours.actions[Schwarma::ATTACK] +
+                this->behaviours.actions[Schwarma::DEFEND]))
+                    return Schwarma::DEFEND;
+                if(percent < (this->behaviours.actions[Schwarma::ATTACK] +
+                this->behaviours.actions[Schwarma::DEFEND] + 
+                this->behaviours.actions[Schwarma::MOVE]))
+                    return Schwarma::MOVE;
             }
     };
 }
