0f9f8c77ed5757f37e6ac57d92854689deb2e42a
Chris Gibb <chris.gibb@outlook.com>
2017-02-15 17:33:31 -0500

    Removed redundant position setting

--- simServer/main.cpp
+++ simServer/main.cpp
@@ -18,9 +18,6 @@ int main(int argc,char*argv[])
     player1.stats.health = 10;
     player2.stats.health = 10;
 
-    //Set players on opposite ends of the stage
-    player1.position = 1;
-    player2.position = 7;
 
     player1.name = "Player 1";
     player2.name = "Player 2";
