aeda75b0bda0e8f8058073d18042c728a4fb66cc
Chris Gibb <chris.gibb@outlook.com>
2017-02-19 22:33:36 -0500

    Remove old comments and add new

--- simServer/main.cpp
+++ simServer/main.cpp
@@ -14,7 +14,6 @@ int main(int argc,char*argv[])
     //see rnadom number generator for weighted decision making
     ::srand(::time(NULL));
 
-    //Setup some dummy player objects to test with
     Schwarma::Player player1;
     Schwarma::Player player2;
 
@@ -36,11 +35,12 @@ int main(int argc,char*argv[])
     player1.position = 1;
     player2.position = 6;
 
-    //Setup a new simulation with our two players
     Schwarma::Simulation sim(&player1,&player2);
 
+    //If arg 3 is cout then pipe to stdout
     if(argc >= 4 && (::strcmp(argv[3],"cout") == 0))
         sim.run<decltype(std::cout)>(std::cout);
+    //Otherwise interpret arg 3 as a file to pipe to
     else
     {
         std::ofstream stream(argv[3],std::ios::out|std::ios::trunc);
