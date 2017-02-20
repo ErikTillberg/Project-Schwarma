5661b232e71e8b1a06b6e765b71f92b3e540cae4
Chris Gibb <chris.gibb@outlook.com>
2017-02-19 22:28:27 -0500

    Added error handling to json loading and optional stdout redirection

--- simServer/main.cpp
+++ simServer/main.cpp
@@ -20,15 +20,26 @@ int main(int argc,char*argv[])
     player1.name = "Player 1";
     player2.name = "Player 2";
 
-    player1.loadFromFile(argv[1]);
-    player2.loadFromFile(argv[2]);
+    bool res = player1.loadFromFile(argv[1]);
+    if(!res)
+    {
+        std::cout<<"Could not load "<<argv[1]<<std::endl;   
+        return 1;
+    }
+    res = player2.loadFromFile(argv[2]);
+    if(!res)
+    {
+        std::cout<<"Could not load "<<argv[2]<<std::endl;
+        return 1;
+    }
     player1.position = 1;
     player2.position = 6;
 
     //Setup a new simulation with our two players
     Schwarma::Simulation sim(&player1,&player2);
-    //Run the simulation (infinite loop for now) and pipe results to console 
-    sim.run<decltype(std::cout)>(std::cout);
+
+    if(argc >= 4 && (::strcmp(argv[3],"cout") == 0))
+        sim.run<decltype(std::cout)>(std::cout);
     
     return 0;
 }
