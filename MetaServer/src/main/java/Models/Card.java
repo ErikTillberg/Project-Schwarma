a0c3a8c970f2513031f5bd77ba81940f17b6abdb
p24601 <cervellini@gmail.com>
2017-02-22 21:06:10 -0800

    Equipment generation function and initial inventory

--- MetaServer/src/main/java/Models/Card.java
+++ MetaServer/src/main/java/Models/Card.java
@@ -150,6 +150,7 @@ public class Card {
         return type.equals(Card.ATTACK) || type.equals(Card.DEFENSE) || type.equals(Card.MOBILITY);
     }
 
+
     /////////////////////////////////////////
     ///////////GETTER AND SETTERS////////////
     /////////////////////////////////////////
