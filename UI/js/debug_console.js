674486df620cdd460f1bf5542a1db8c8033babd5
Andrew Vester <ajrvester@gmail.com>
2017-03-02 19:19:20 -0500

    Updated comments to work with JSDoc.
    Added JSDoc output files to the Documentation/UI folder.

--- UI/js/debug_console.js
+++ UI/js/debug_console.js
@@ -2,26 +2,29 @@
  * Created by Andrew on 2017-02-09.
  */
 
-// Displays simple messages at the bottom of the canvas for the user/developer.
-// Currently, must be initialized inside each state. Must be initialized after
-// Drawing any bakcgrounds so text does not wind up in the background
-// TODO make text float to top on init so invocation order doesn't matter
-// TODO see if there is a way to persists the text element between states and implement that
-
-console.log("debug_log");
-
+/**
+ * Displays simple messages at the bottom of the canvas for the user/developer. Currently, must be initialized inside each state. Must be initialized after drawing
+ * any backgrounds so text does not wind up in the background.
+ * @namespace
+ * @type {{debug_mode: boolean, font_string: string, init_log: debug_console.init_log, debug_log: debug_console.debug_log, message_log: debug_console.message_log, error_log: debug_console.error_log}}
+ */
 var debug_console = {
 
     debug_mode: true,
     font_string: "14px Arial",
 
-    // Builds a text box inside the canvas each state can write messages to
+    /**
+     * Builds a Phaser game text object inside the canvas for message writing.
+     */
     init_log: function(){
         console.log("debug_console: init_log");
         this.output_box = game.add.text(40, 680, "");
     },
 
-    // Outputs the message to the canvas if debug_mode is true, otherwise writes it to the js console
+    /**
+     * Outputs the message to the canvas if debug_mode is true, otherwise writes it to the js console.
+     * @param {String} message
+     */
     debug_log: function(message){
 
         if (this.debug_mode){
@@ -32,7 +35,10 @@ var debug_console = {
         }
     },
 
-    // Outputs an info message on the canvas for the user to see
+    /**
+     * Outputs the message to the canvas.
+     * @param {String} message
+     */
     message_log: function(message){
         console.log("debug_console: message_log");
         this.output_box.setText(message);
@@ -40,7 +46,10 @@ var debug_console = {
 
     },
 
-    // Outputs an error message on the canvas for the user to see
+    /**
+     * Outputs an error message to the canvas.
+     * @param {String} message
+     */
     error_log: function(message){
         this.output_box.setText(message);
         this.output_box.setStyle({font: this.font_string, fill: "red"});
--- UI/js/user.js
+++ UI/js/user.js
@@ -1,10 +1,18 @@
 /**
  * Created by Andrew on 2017-02-09.
  *
- * user.js - Holds the user's data that has been sent to the client.
  */
 
+/**
+ * Singleton Object holds client user data that must persist between game states.
+ * @type {{init: user.init, init_opponent: user.init_opponent, opponent: {username: string, character_type: string, rating: number}}}
+ * @namespace
+ */
 var user = {
+    /**
+     * Initializes data of the user.
+     * @param {Object} user_data JSON object from meta-server with user information.
+     */
     init: function(user_data){
         this.username = user_data.username;
         this.email = user_data.email;
@@ -14,11 +22,18 @@ var user = {
         this.character_type = user_data.characterType;
         this.rating = user_data.rating;
     },
+    /**
+     * Initializes data of the current opponent.
+     * @param {Object} opponent_data JSON object from meta-server with opponent information.
+     */
     init_opponent: function(opponent_data){
         this.opponent.username = opponent_data.username;
         this.opponent.character_type = opponent_data.characterType;
         this.opponent.rating = opponent_data.rating;
     },
+    /**
+     * Holds data of the current opponent.
+     */
     opponent: {
         username: "Player2", // Default which shows up inside the battle test, overwritten when match is found
         character_type: 'thief',
