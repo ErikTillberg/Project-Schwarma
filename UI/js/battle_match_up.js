676fdfd371ef5343de696d1f463b49f5e4ab1827
bignoyrb <noyrb@live.ca>
2017-03-28 20:20:50 -0400

    Added match up screen

---
+++ UI/js/battle_match_up.js
@@ -0,0 +1,136 @@
+
+
+var sword;
+var playerCharcter;
+var playerCharcter2;
+var playerCharacterShadow;
+var playerCharacterShadow2;
+
+/**
+ * Handles the character match up screen.
+ * namespace
+ * @type {{preload: pick_state.preload, create: pick_state.create, back_btn_click: pick_state.back_btn_click, submit_btn_click: pick_state.submit_btn_click}}
+ */
+var match_up_state = {
+ 
+    preload: function() {
+
+       console.log("match_up_state: preload");
+    },
+
+    create: function() {
+
+
+        // Set the background color of the canvas.
+        game.stage.backgroundColor = 'rgb(255, 255, 255)';
+
+        // Add the menu background to screen
+        var background = game.add.sprite(0,0, 'menu_background');
+        var banner = game.add.sprite(640,210,'Banner');
+        banner.frame = randomInt(0, 4);
+        banner.anchor.setTo(0.5, 0.5);
+
+        // Add the character sprite and add animations
+        playerCharcter = game.add.sprite(1020, 400,  pickCharacter ( user.opponent.character_type));
+        playerCharcter.anchor.setTo(0.5, 0.5);
+        playerCharcter.scale.setTo(1.5,1.5);
+        playerCharacterShadow = game.add.sprite(playerCharcter.x, playerCharcter.y + 42, 'Shadow');
+        playerCharacterShadow.anchor.setTo(0.5, 0.5);
+        playerCharacterShadow.scale.setTo(1.5,1.5);
+        var walk = playerCharcter.animations.add('walk');
+        playerCharcter.animations.play('walk', 5, true);
+
+
+        var char1StatText = game.add.bitmapText(playerCharcter.x, playerCharcter.y + 140, 'carrier_command_black','ATTACK: 3\n\nMOBILITY: 1\n\nDEFENCE: 2',20);
+        char1StatText.anchor.setTo(0.5, 0.5);
+        char1StatText.align = 'center';
+
+
+        playerCharcter2 = game.add.sprite(260, 400, pickCharacter ( user.character_type));
+        playerCharcter2.anchor.setTo(0.5, 0.5);
+        playerCharcter2.scale.setTo(1.5,1.5);
+        playerCharacterShadow2 = game.add.sprite(playerCharcter2.x, playerCharcter2.y + 42, 'Shadow');
+        playerCharacterShadow2.anchor.setTo(0.5, 0.5);
+        playerCharacterShadow2.scale.setTo(1.5,1.5);
+        var walk = playerCharcter2.animations.add('walk');
+        playerCharcter2.animations.play('walk', 5, true);
+
+        var char2StatText = game.add.bitmapText(playerCharcter2.x, playerCharcter2.y + 140, 'carrier_command_black','ATTACK: 1\n\nMOBILITY: 2\n\nDEFENCE: 3',20);
+        char2StatText.anchor.setTo(0.5, 0.5);
+        char2StatText.align = 'center';
+
+
+        // Add the title text to the screen                
+        var titleText = game.add.bitmapText(banner.x, banner.y - 100, 'carrier_command_black','BATTLE MATCH UP',40);
+        titleText.anchor.setTo(0.5, 0.5);
+        titleText.align = 'center';
+
+        var vsText = game.add.bitmapText(640, 360, 'carrier_command_black','VS',60);
+        vsText.anchor.setTo(0.5, 0.5);
+        vsText.align = 'center';
+
+        var user1Text = game.add.bitmapText(playerCharcter.x -100, playerCharcter.y - 200, 'carrier_command_black',user.opponent.username,25);
+        vsText.anchor.setTo(0.5, 0.5);
+        vsText.align = 'center';
+
+        var user2Text = game.add.bitmapText(playerCharcter2.x -100, playerCharcter2.y - 200, 'carrier_command_black',user.username,20);
+        vsText.anchor.setTo(0.5, 0.5);
+        vsText.align = 'center';
+
+
+        // Add a submit button to the screen, and when clicked launch submit_button_click function
+        this.submit_btn = game.add.button(1120, 600,  'Submit_button', this.submit_btn_click, this, 2, 1, 0);
+
+        // Add a home button to the screen, and when clicked launch back_btn_clickfunction
+        this.back_btn= game.add.button(50, 600,  'Home_button', this.back_btn_click, this, 2, 1, 0);
+    },
+
+    // Handles back button click. Transitions the game to the load state.
+    back_btn_click: function(){
+
+        console.log("signup_state: signin_btn_click");
+        game.state.start("load");
+    },
+
+    // Handles submit button click. Transitions the game to the signup state.
+    submit_btn_click: function(){
+
+         game.state.start("pre_battle");
+    },
+};
+
+
+/**
+* Function creates a random integer between min and max and returns it.
+* @param min
+* @param max
+* @returns {integer}
+*/
+ function randomInt (min, max) {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * Function picks what character sprite to load.
+ * @param charName
+ * @returns {string}
+ */
+function pickCharacter ( charName ){
+
+    if (charName == 'warrior'){
+        return 'Knight';
+    }
+
+    else if (charName == 'thief'){
+        return 'Thief';
+    }
+
+    else {
+        return 'Wizard';
+    }
+}
+
+
--- UI/js/boot.js
+++ UI/js/boot.js
@@ -18,6 +18,7 @@ game.state.add('main_menu', main_menu_state);
 game.state.add('gear_menu', gear_menu_state);
 game.state.add('battle_system', battle_system_state);
 game.state.add('pre_battle', pre_battle_state);
+game.state.add('battle_match_up',  match_up_state);
 
 // Start the load state to import game assets
 game.state.start("load");
